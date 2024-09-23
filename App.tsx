10de8d0b5dc5396d04844daf9c219e5ce8d2be1f
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-09-23 22:48:50 +0500

    push

--- App.tsx
+++ App.tsx
@@ -77,6 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
+            Syed farrukh ahmed
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
