86ab50a7054c108ae00c1cad67cb3dd1ff3e7367
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-09-24 01:49:31 +0500

    push geolocation configuration android and ios both

--- src/features/auth/SplashScreen.tsx
+++ src/features/auth/SplashScreen.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React, { FC } from 'react'
+import { Colors } from '@utils/Constants'
+import { screenHeight, screenWidth } from '@utils/Scaling'
+import Logo from '@assets/images/splash_logo.jpeg'
 
-const SplashScreen = () => {
+const SplashScreen: FC = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+        <Image source={Logo} style={styles.logoImage} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.primary,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logoImage: {
+        height: screenHeight * 0.7,
+        width: screenWidth * 0.7,
+        resizeMode: 'contain',
+    }
+})
 
 export default SplashScreen;
