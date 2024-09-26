73500f67cc2ff1dbddba40d7aa32fe71b24b241c
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-09-27 04:55:59 +0500

    Push customer login screen with slider

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -2,12 +2,13 @@ import { View, Text, StyleSheet } from 'react-native'
 import React, { FC } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import CustomSafeAreaView from '@components/global/CustomSafeAreaView'
+import ProductSlider from '@components/login/ProductSlider'
 
 const CustomerLogin: FC = () => {
   return (
     <GestureHandlerRootView style={styles.container}>
         <CustomSafeAreaView>
-          <Text> Ahmed </Text>
+          <ProductSlider />
         </CustomSafeAreaView>
     </GestureHandlerRootView>
   )
