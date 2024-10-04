fbc633493e719ddca4587e47df23ed348348ede3
syedfarrukh0 <ssyedfarrukh0@gmail.com>
2024-10-05 01:23:56 +0500

    push category sidebar on cateries page

--- src/features/category/ProductCategories.tsx
+++ src/features/category/ProductCategories.tsx
@@ -1,12 +1,70 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import React, {FC, useEffect, useState} from 'react';
+import CustomHeader from '@components/ui/CustomHeader';
+import {Colors} from '@utils/Constants';
+import Sidebar from './Sidebar';
+import { getAllCategories } from '@service/productService';
 
-const ProductCategories = () => {
+const ProductCategories: FC = () => {
+  const [categories, setCategories] = useState<any[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<any>(null);
+  const [products, setProducts] = useState<any[]>([]);
+  const [categoriesLoading, setCategoriesLoading] = useState<boolean>(true);
+  const [productsLoading, setProductsLoading] = useState<boolean>(false);
+
+  const fetchCategories = async () => {
+    try {
+      setCategoriesLoading(true);
+      const data = await getAllCategories();
+      // console.log(data)
+      setCategories(data?.data);
+      if (data && data.length > 0) {
+        setSelectedCategory(data[0]);
+      }
+    } catch (error) {
+      console.log('Error fetching categories: ', error) 
+    }finally{
+      setCategoriesLoading(false);
+    }
+  }
+
+  useEffect(()=>{
+    fetchCategories();
+  },[])
+
   return (
-    <View>
-      <Text>ProductCategories</Text>
+    <View style={styles.mainContainer}>
+      <CustomHeader title={selectedCategory?.name || 'categories'} search />
+      <View style={styles.subContainer}>
+        {categoriesLoading ? (
+          <ActivityIndicator size={'small'} color={Colors.border} />
+        ) : (
+          <Sidebar
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onCategoryPress={(category:any) => setSelectedCategory(category)}
+          />
+        )}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default ProductCategories
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  subContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default ProductCategories;
---
+++ src/service/productService.tsx
@@ -0,0 +1,62 @@
+import {BASE_URL} from './config';
+import axios from 'axios';
+
+export const getAllCategories = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/categories`);
+    // Return success response
+    return {success: true, data: res.data} as any;
+    // return res.data;
+  } catch (error: any) {
+    let errorResponse = {
+      success: false,
+      message: 'An error occurred',
+      status: 0,
+      errorData: null,
+    };
+    if (error.response) {
+      // Server responded with a status code other than 2xx
+      errorResponse.status = error.response.status;
+      errorResponse.errorData = error.response.data;
+      errorResponse.message = error.response.data.message || 'Server Error';
+    } else if (error.request) {
+      // The request was made but no response was received
+      errorResponse.message = 'Network Error: No response from server';
+    } else {
+      // Something else happened
+      errorResponse.message = `Request error: ${error.message}`;
+    }
+    // Ensure error details are returned
+    return {errorResponse: errorResponse} as any;
+  }
+};
+
+export const getProductsByCategoryId = async (id: string) => {
+  try {
+    const res = await axios.get(`${BASE_URL}/products/${id}`);
+    // Return success response
+    console.log(res)
+    return {success: true, data: res} as any;
+  } catch (error: any) {
+    let errorResponse = {
+      success: false,
+      message: 'An error occurred',
+      status: 0,
+      errorData: null,
+    };
+    if (error.response) {
+      // Server responded with a status code other than 2xx
+      errorResponse.status = error.response.status;
+      errorResponse.errorData = error.response.data;
+      errorResponse.message = error.response.data.message || 'Server Error';
+    } else if (error.request) {
+      // The request was made but no response was received
+      errorResponse.message = 'Network Error: No response from server';
+    } else {
+      // Something else happened
+      errorResponse.message = `Request error: ${error.message}`;
+    }
+    // Ensure error details are returned
+    return {errorResponse: errorResponse} as any;
+  }
+};
